38231a33af7872b6538cef5e1130199d39bb06ec
marcel_corell <marcel.corell@basf.com>
2021-06-01 08:18:46 +0200

    adjust new list to do model validation

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,20 +19,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-if 'DJANGO_DEBUG_FALSE' in os.environ:
+if "DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["SITENAME"]]
 else:
     DEBUG = True
     SECRET_KEY = "2vadqmjt0g@m4$1%8)&*o8=$7u^m!6hz#8xszgu5fee^-3g&$x"
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    #"django.contrib.admin",
+    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
