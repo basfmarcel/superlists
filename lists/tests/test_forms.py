b2f084812991e635a75bdfd60452cb067361060c
marcel_corell <marcel.corell@basf.com>
2021-06-02 17:12:08 +0200

    prepare to pull to macbook

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -9,9 +9,6 @@ class ItemFormTest(TestCase):
         self.assertIn('class="form-control input-lg"', form.as_p())
 
     def test_form_validation_for_blank_items(self):
-        form = ItemForm(data={'text': ''})
+        form = ItemForm(data={"text": ""})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            [EMPTY_ITEM_ERROR]
-        )
+        self.assertEqual(form.errors["text"], [EMPTY_ITEM_ERROR])
