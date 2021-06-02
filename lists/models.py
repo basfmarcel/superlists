0c9788cb122ec6890615343375a4a5489cd84aae
marcel_corell <marcel.corell@basf.com>
2021-06-02 08:32:16 +0200

    new form for list items

--- lists/models.py
+++ lists/models.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 class List(models.Model):
     def get_absolute_url(self):
-        return reverse('view_list', args=[self.id])
+        return reverse("view_list", args=[self.id])
 
 
 class Item(models.Model):
