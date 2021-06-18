5c1148d14ad824c66ffc68527d2f9665c4dba0f4
Marcel Corell <marcel.corell.94@gmail.com>
2021-06-18 12:59:45 +0200

    throubleshooting FTs: Import of wait from FunctionalTest

--- functional_tests/lists_page.py
+++ functional_tests/lists_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.keys import Keys
-from .base import wait
+from .base import FunctionalTest
 
 
 class ListPage(object):
@@ -9,14 +9,14 @@ class ListPage(object):
     def get_table_rows(self):
         return self.test.browser.find_elements_by_css_selector("#id_list_table tr")
 
-    @wait
+    @FunctionalTest.wait
     def wait_for_row_in_list_table(self, item_text, item_number):
         expected_row_text = f"{item_number}: {item_text}"
         rows = self.get_table_rows()
         self.test.assertIn(expected_row_text, [row.text for row in rows])
 
     def get_item_input_box(self):
-        return self.browser.find_element_by_id("id_text")
+        return self.test.browser.find_element_by_id("id_text")
 
     def add_list_item(self, item_text):
         new_item_no = len(self.get_table_rows()) + 1
