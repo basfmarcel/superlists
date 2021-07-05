b7f9ed65a834a66397dce0fc3838217a60d6d681
Marcel Corell <marcel.corell.94@gmail.com>
2021-07-05 22:43:59 +0200

    local setup running

--- superlists/settings.py
+++ superlists/settings.py
@@ -107,8 +107,11 @@ WSGI_APPLICATION = "superlists.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db", # set in docker compose
+        "PORT": 5432 # default postgres port
     }
 }
 
