b55aa8a798338a3c7363ca52ace09df149f6acfb
Marcel Corell <marcel.corell.94@gmail.com>
2021-07-07 15:54:27 +0200

    adapted db settings for dev env

--- superlists/settings.py
+++ superlists/settings.py
@@ -97,15 +97,23 @@ WSGI_APPLICATION = "superlists.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "HOST": "db",  # set in docker compose
-        "PORT": 5432,  # default postgres port
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "HOST": "db",  # set in docker compose
+            "PORT": 5432,  # default postgres port
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
